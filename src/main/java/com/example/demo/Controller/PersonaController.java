3d521f1958adeb50850130735b6179b3a1fe087f
Tomas <tomasdomingoxxi@gmail.com>
2023-05-29 15:27:24 -0300

    reparacion de errores

--- src/main/java/com/example/demo/Controller/PersonaController.java
+++ src/main/java/com/example/demo/Controller/PersonaController.java
@@ -22,6 +22,7 @@ import com.example.demo.Entity.Persona;
 import com.example.demo.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,7 +36,10 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author mx
  */
+
 @RestController
+@CrossOrigin(origins ="http://localhost:4200 https://trabajo-final-3b194.web.app") 
+
 public class PersonaController {
 
   
@@ -78,6 +82,11 @@ public class PersonaController {
    
       
    }
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        
+        return ipersonaService.findPersona((long) 1);
+    }
 }
 
     
